8cfaff1d8e00bfc4ea560c4c0838873ba559290d
luisacavalcante <38131887+luisacavalcante@users.noreply.github.com>
2023-07-24 22:33:45 -0300

    wifi+ hora+ angulos+ salvar_memoria
    Falta enviar pra nuvem e rodar dados coletados na IA

--- leitura_angulos.ino
+++ leitura_angulos.ino
@@ -1,3 +1,9 @@
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <time.h>
+#define LED_PIN 2
+
 // Basic demo for accelerometer readings from Adafruit MPU6050
 
 // ESP32 Guide: https://RandomNerdTutorials.com/esp32-mpu-6050-accelerometer-gyroscope-arduino/
@@ -10,18 +16,78 @@
 
 Adafruit_MPU6050 mpu;
 double gravity = 9.89;
-
 double ax, ay, az;
 double gx, gy, gz;
-void setup(void) {
+String n = "\n";
+String data_hora = "";
+
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = -10800; // UTC-3 (three hours behind UTC)
+const int daylightOffset_sec = 3600;
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, ntpServer, gmtOffset_sec);
+
+
+//CARTAO MEMORIA
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+
+void setup() {
   Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  WiFi.begin("LIVE TIM_1901_2G", "danivalberlu");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  timeClient.begin();
+  inicializar_sdcard();
+  setup_mpu();
+}
 
-  Serial.println("Adafruit MPU6050 test!");
+void loop() {
+    get_hora();
+    //colocar leitura do mpu
+    get_mpu();
+    delay(500);
+}
+void inicializar_sdcard(){
+  
+    if(!SD.begin()){
+        Serial.println("Card Mount Failed");
+        return;
+    }
+    uint8_t cardType = SD.cardType();
 
-  // Try to initialize!
-  if (!mpu.begin()) {
+    if(cardType == CARD_NONE){
+        Serial.println("No SD card attached");
+        return;
+    }
+
+    
+  
+  }
+void get_mpu(){
+   sensors_event_t a, g, temp;
+   mpu.getEvent(&a, &g, &temp);
+   ax=a.acceleration.x;
+   ay=a.acceleration.y;
+   az= a.acceleration.z -gravity;
+   gx=g.gyro.x;
+   gy=g.gyro.y;
+   gz=g.gyro.z;
+
+  
+  String value =  "";
+  value = data_hora+","+String(ax)+","+String(ay)+","+String(az)+","+String(gx)+","+String(gy)+","+String(gz)+n;
+  Serial.println(value);
+  appendFile(SD, "/dados_tcc.txt",value);
+}
+
+void setup_mpu(){
+if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
       delay(10);
@@ -88,44 +154,93 @@ void setup(void) {
     break;
   }
 
-  Serial.println("");
-  delay(100);
 }
 
-void loop() {
-  /* Get new sensor events with the readings */
- 
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-ax=a.acceleration.x;
- ay=a.acceleration.y;
- az= a.acceleration.z -gravity;
- gx=g.gyro.x ;
- gy=g.gyro.y;
- gz=g.gyro.z;
+void get_hora(){
+  timeClient.update();
 
+  // Get Unix timestamp
+  unsigned long epochTime = timeClient.getEpochTime();
 
-  /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(ax);
-  Serial.print(", Y: ");
-  Serial.print(ay);
-  Serial.print(", Z: ");
-  Serial.print(az);
-  Serial.println(" m/s^2");
+  // Convert to time struct
+  struct tm *timeinfo;
+  time_t rawTime = (time_t)epochTime;
+  timeinfo = localtime(&rawTime);
 
-  Serial.print("Rotation X: ");
-  Serial.print(gx);
-  Serial.print(", Y: ");
-  Serial.print(gy);
-  Serial.print(", Z: ");
-  Serial.print(gz);
-  Serial.println(" rad/s");
+  // Extract date and time components
+  int day = timeinfo->tm_mday;
+  int month = timeinfo->tm_mon + 1; // Months are 0-based
+  int year = timeinfo->tm_year + 1900; // Years since 1900
+  String formattedTime = timeClient.getFormattedTime();
 
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");
+  // Print date and time (adjusted to local time)
+  data_hora="";
+  data_hora=data_hora+year+"-";
+  if (month < 10){
+    data_hora=data_hora+"0";
+  }
+  data_hora=data_hora+String(month)+"-";
+  if (day < 10){
+     data_hora=data_hora+"0";
+  } 
+   data_hora=data_hora+String(day)+" ";
+   data_hora=data_hora+formattedTime;
+  
 
-  Serial.println("");
-  delay(500);
 }
+
+
+
+void appendFile(fs::FS &fs, String path, String message){
+    //Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        digitalWrite(LED_PIN, HIGH);
+        return;
+    }
+    if(file.print(message)){
+      digitalWrite(LED_PIN, LOW);
+      //  Serial.println("Message appended");
+    } else {
+        digitalWrite(LED_PIN, HIGH);
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+void listDir(fs::FS &fs, String dirname, uint8_t levels){
+    //Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+           // Serial.print("  DIR : ");
+            //Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.path(), levels -1);
+            }
+        } else {
+          //  Serial.print("  FILE: ");
+          //  Serial.print(file.name());
+          //  Serial.print("  SIZE: ");
+          //  Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+
+
+
