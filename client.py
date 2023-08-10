b6dd1c2ab6c0a73cfa59cfefd8604ae57fa2aab2
luisacavalcante <lncc@cin.ufpe.br>
2023-08-10 19:07:57 -0300

    Ajuste para requisição correta (sem ser de teste)

--- client.py
+++ client.py
@@ -1,182 +1,32 @@
 import requests
 
 # URL of the Flask API on the server
-api_url = 'https://da53-35-194-216-133.ngrok-free.app/get_data'  # Replace with your server's IP
-#api_url = 'https://da53-35-194-216-133.ngrok-free.app//teste'  # Replace with your server's IP
+api_url = 'http://61c8-35-225-43-53.ngrok-free.app/get_data'  # Replace with your server's IP
+#api_url = 'http://4cfe-35-225-43-53.ngrok-free.app/teste'  # Replace with your server's IP
 try:
     # Sample data to send to the server
+
+
     data_to_send = [
+
         {
-            "AccX": 0.667560,
-            "AccY": -0.038610,
-            "AccZ": 0.231416,
-            "GyroX": -0.054367,
-            "GyroY": -0.007712,
-            "GyroZ": 0.225257
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
-        },
-        {
-            "AccX": 0.123456,
-            "AccY": -0.789012,
-            "AccZ": 0.345678,
-            "GyroX": -0.901234,
-            "GyroY": -0.567890,
-            "GyroZ": 0.123456
+            "AccX": 0.09,
+            "AccY": 0.34,
+            "AccZ": 0.29,
+            "GyroX": 0.00,
+            "GyroY": 0.02,
+            "GyroZ": -0.02
         },
 
     ]
 
     # Send a POST request to the Flask API with the data as a JSON payload
     #data_teste = {"Teste":"Oi"}
-    #response = requests.post(api_url, json=data_teste)
     response = requests.post(api_url,json=data_to_send)
     if response.status_code == 200:
         try:
-            predicted_class = response.json()['Predicted Class']
-            print('Predicted Class:', predicted_class)
+            #predicted_class = response.json()['Predicted Class']
+           # print('Predicted Class:', predicted_class)
             print(response.text)
         except ValueError:
             print('Error: Invalid JSON response')
